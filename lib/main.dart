eefcaec45d9898e18c088dfbef1e73dca521ad2e
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2025-09-28 09:18:30 +0700

    Tahap 1 (#8)
    * Feat/register screen (#2)
    
    * Pembuatan Halaman Register (tanpa logika)
    
    * Pembuatan Halaman Register (tanpa logika) fix
    
    * feat: add splash screen and setup navigation route
    
    * feat: add login screen and adjust register screen layout based on Figma
    
    * feat: add otp verification pop up
    
    ---------
    
    Co-authored-by: hel-sh <helmishidqi@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:warga_kita_app/screen/login_screen.dart';
+import 'package:warga_kita_app/screen/register_screen.dart';
+import 'package:warga_kita_app/screen/splash_screen.dart';
 import 'package:warga_kita_app/style/theme/wargakita_theme.dart';
 
 void main() {
@@ -14,58 +17,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'WargaKita App',
       theme: WargakitaTheme.mainTheme,
-      home: const HomePage(),
-    );
-  }
-}
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+      // nama route untuk navigasi
+      initialRoute: '/',
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("WargaKita Home")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Text pakai style dari theme
-            Text(
-              "Judul dengan HeadlineLarge",
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            Text(
-              "Ini bodyMedium dengan warna hitam",
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-
-            const SizedBox(height: 24),
-
-            // Tombol Primary (biru)
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                foregroundColor: Theme.of(context).colorScheme.onPrimary,
-              ),
-              onPressed: () {},
-              child: const Text("Tombol Primary (Biru)"),
-            ),
-
-            const SizedBox(height: 12),
-
-            // Tombol Secondary (oranye)
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.secondary,
-                foregroundColor: Theme.of(context).colorScheme.onSecondary,
-              ),
-              onPressed: () {},
-              child: const Text("Tombol Secondary (Oranye)"),
-            ),
-          ],
-        ),
-      ),
+      // daftar route
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+      },
     );
   }
 }
