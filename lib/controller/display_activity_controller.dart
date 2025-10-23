1ae13a29e28f308fb7fb4406e013acc38eb792bd
hel-sh <helmishidqi@gmail.com>
2025-10-23 17:44:04 +0700

    Delete lib/controller/display_activity_controller.dart
    file ini seharusnya tidak ada, karena kemarin ada conflict jadi muncul lagi dari versi sebelumnya

--- lib/controller/display_activity_controller.dart
+++
@@ -1,9 +0,0 @@
-import '../data/activity_model.dart';
-import '../service/display_activity_service.dart';
-
-class DisplayActivityController {
-  final DisplayActivityService _service = DisplayActivityService();
-
-  Stream<List<ActivityModel>> get activitiesStream =>
-      _service.streamAllActivities();
-}
